ca35189cb43295038bfae0f857224456866dd0a5
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2025-12-02 16:24:05 -0500

    Delete obsolete files

--- public/scripts/parseDuration.js
+++
@@ -1,7 +0,0 @@
-const parseDuration = str => {
-  const match = str.match(/^(\d+)(d)$/);
-  if (!match) throw new Error('Invalid duration');
-  return parseInt(match[1]) * 24 * 60 * 60 * 1000;
-};
-
-module.exports = parseDuration;
