8f392441fdd9ad30869403b015d9c71fabc6e8e7
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2025-12-02 16:15:17 -0500

    Add prisma config file to align with v7.0.1

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config"; // ensures DATABASE_URL is loaded
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",   // explicitly point to your schema file
+  migrations: {
+    path: "prisma/migrations",      // optional, but recommended
+  },
+  datasource: {
+    url: env("DATABASE_URL"),       // use env() helper, not process.env directly
+  },
+});
